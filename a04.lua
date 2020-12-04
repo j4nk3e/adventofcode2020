d63af6a6e2c09d6327cfa7d76f16b9bbd417928b
Julian Köpke <julian.koepke@gmail.com>
2020-12-04 23:56:02 +0100

    a04 block

--- a04.lua
+++ a04.lua
@@ -1,9 +1,18 @@
-function o(s,a,b)return s-a>0 and s-a<b end c={byr=function(s)return
-o(s,1919,84)end,iyr=function(s)return o(s,2009,12)end,eyr=function(s)return
-o(s,2019,12)end,hgt=function(s)e,g=s:gmatch'(%d+)(%a+)'()return
-g=='cm'and o(e,149,45)or g=='in'and o(e,58,19)end,hcl=function(s)return
-s:find'#[%da-f]+'and#s==7 end,pid=function(s)return#s==9 and
-s:find'%d+'end,ecl=function(s)return({amb=1,blu=1,brn=1,gry=1,grn=1,hzl=1,oth=1})[s]end
-}q,d,x=0,0,io.read'*a'function w()for id,p in pairs(c)do r=a:gmatch(id..':(%S+)')()if
-not r or not p(r)then return 0 end end end while q do f,t=x:find('\n\n',q)a,q=x:sub(q,f
-and t),t d=d+(w()or 1)end print(d)
+function o(a, b) return function(s)
+return (s-a>0)and s-a<b end end c={
+hgt=function(s) e,g=s:gmatch('(%d+'
+..')(%a+)')() return g=='cm' and o(
+149,45)(e) or g=='in' and o(58,19)(
+e) end, eyr=o(2019,12), byr=o(1919,
+84), hcl=function(s) return s:find(
+'#[%da-f]+') and #s == 7 end,iyr=o(
+2009,12), pid=function(s) return #s
+==9 and s:find'%d+'end,ecl=function
+(s) return({amb=1,blu=1,brn=1,gry=1
+, grn=1, hzl=1, oth=1}) [s] end} q,
+d=0, 0 x=io.read('*a') function w()
+for h, p in pairs(c) do r=a:gmatch(
+h..':(%S+)')() if not r or not p(r)
+then return (0) end end end while q
+do f,t=x:find('\n\n',q)q,a=t,x:sub(
+q,f and t)d=d+(w()or 1)end print(d)
