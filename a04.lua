4c9a63a4ea295c1aee12af1f18c2819771d89c32
Julian Köpke <julian.koepke@gmail.com>
2020-12-04 23:09:21 +0100

    a04 shorter

--- a04.lua
+++ a04.lua
@@ -1,7 +1,7 @@
 function o(s,a,b)return s-a>0 and s-a<b end c={byr=function(s)return
 o(s,1919,84)end,iyr=function(s)return o(s,2009,12)end,eyr=function(s)return
-o(s,2019,12)end,hgt=function(s)e=s:gmatch'(%d+)cm'()g=s:gmatch'(%d+)in'()return
-e and o(e,149,45)or g and o(g,58,19)end,hcl=function(s)return
+o(s,2019,12)end,hgt=function(s)e,g=s:gmatch'(%d+)(%a+)'()return
+g=='cm'and o(e,149,45)or g=='in'and o(e,58,19)end,hcl=function(s)return
 s:find'#[%da-f]+'and#s==7 end,pid=function(s)return#s==9 and
 s:find'%d+'end,ecl=function(s)return({amb=1,blu=1,brn=1,gry=1,grn=1,hzl=1,oth=1})[s]end
 }q,d,x=0,0,io.read'*a'function w()for id,p in pairs(c)do r=a:gmatch(id..':(%S+)')()if
