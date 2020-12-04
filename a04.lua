1bcbd5518a49a322fd0619b746b8501ab051963b
Julian Köpke <julian.koepke@gmail.com>
2020-12-04 22:16:18 +0100

    a04 golf

--- a04.lua
+++ a04.lua
@@ -1,45 +1,9 @@
-function inrange(s, a, b)
-    local i = tonumber(s)
-    return i >= a and i <= b
-end
-req = {
-    byr = function(s) return inrange(s, 1920, 2002) end,
-    iyr = function(s) return inrange(s, 2010, 2020) end,
-    eyr = function(s) return inrange(s, 2020, 2030) end,
-    hgt = function(s)
-        for cm in s:gmatch('(%d+)cm') do return inrange(cm, 150, 193) end
-        for ip in s:gmatch('(%d+)in') do return inrange(ip, 59, 76) end
-        return false
-    end,
-    hcl = function(s) return s:find('#[%da-f]+') and #s == 7 end,
-    ecl = function(s)
-        return
-            ({amb = 1, blu = 1, brn = 1, gry = 1, grn = 1, hzl = 1, oth = 1})[s]
-    end,
-    pid = function(s) return #s == 9 and s:find '%d+' end
-}
-q, fail, n = 0, 0, 0
-x = io.read '*a'
-while q do
-    from, to = x:find('\n\n', q)
-    if from then
-        pass = x:sub(q, to)
-    else
-        pass = x:sub(q)
-    end
-    q = to
-    r = {}
-    n = n + 1
-    for l, v in pass:gmatch('(%a+):(%S+)') do r[l] = v end
-    for id, p in pairs(req) do
-        if r[id] and p(r[id]) then
-            print('ok', id, r[id])
-        else
-            print('false', id, r[id])
-            print ''
-            fail = fail + 1
-            break
-        end
-    end
-end
-print('total:', n, 'failed:', fail, 'valid:', n - fail)
+function o(s,a,b)return s-a>0 and s-a<b end c={byr=function(s)return
+o(s,1919,84)end,iyr=function(s)return o(s,2009,12)end,eyr=function(s)return
+o(s,2019,12)end,hgt=function(s)e=s:gmatch'(%d+)cm'()g=s:gmatch'(%d+)in'()return
+e and o(e,149,45)or g and o(g,58,19)end,hcl=function(s)return
+s:find'#[%da-f]+'and#s==7 end,pid=function(s)return#s==9 and
+s:find'%d+'end,ecl=function(s)return({amb=1,blu=1,brn=1,gry=1,grn=1,hzl=1,oth=1})[s]end
+}q,d,x=0,0,io.read'*a'function w()for id,p in pairs(c)do r=a:gmatch(id..':(%S+)')()if
+not r or not p(r)then return 0 end end end while q do f,t=x:find('\n\n',q)a,q=x:sub(q,f
+and t),t d=d+(w()or 1)end print(d)
