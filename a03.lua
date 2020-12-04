c3a4c96c2e07a0a73ed3b14d66f0061a26b7ce65
Julian Köpke <julian.koepke@gmail.com>
2020-12-04 01:27:45 +0100

    a03 even shorter

--- a03.lua
+++ a03.lua
@@ -1,6 +1,5 @@
-k,x=1,{}for l in io.read'*a':gmatch'%p+'do
-w,i,r=#l,0,{}x[k]=r k=k+1 while i do
-i=l:find('#',i+1)r[i or 0]=1 end end function
-u(r,d)i,t=0,0 for c=1+d,#x,d do i=(i+r)%w
-t=x[c][i+1] and t+1 or t end return t end
-print(u(1,1)*u(3,1)*u(5,1)*u(7,1)*u(1,2))
+k,x=1,{}for b in io.read'*a':gmatch'%p+'do
+w,j,v=#b,0,{}x[k]=v k=k+1 while j do j=b:find('#',j+1)v[j
+or 0]=1 end end function u(v,s)t=0 for y=1,#x/s-1
+do t=x[y*s+1][(y*v)%w+1] and t+1 or t end return t
+end print(u(1,1)*u(3,1)*u(5,1)*u(7,1)*u(1,2))
