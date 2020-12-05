4da99595b06ec7121c03807b832c7007be2b23b6
Julian Köpke <julian.koepke@gmail.com>
2020-12-05 11:07:25 +0100

    a05 golf

--- a05.lua
+++ a05.lua
@@ -1,11 +1,4 @@
-m, a = 0, {}
-for l in io.lines() do
-    n = tonumber(l:gsub('[FL]', '0'):gsub('[BR]', '1'), 2)
-    a[n] = 1
-    m = n > m and n or m
-end
-print(m)
-for i, n in pairs(a) do
-    if m == i - 2 then print(m + 1) end
-    m = i
-end
+m,a=0,{}for l in io.lines()do
+n=tonumber(l:gsub('[FL]','0'):gsub('[BR]','1'),2)a[n]=1
+m=n>m and n or m end print(m) for i,n in pairs(a)do if
+m==i-2 then print(m+1)end m=i end
