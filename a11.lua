7c70225af41f176dc3081d74ae53485e9671ede4
Julian Köpke <julian.koepke@gmail.com>
2020-12-12 00:51:59 +0100

    a11 golf

--- a11.lua
+++ a11.lua
@@ -1,49 +1,11 @@
-rows, save = {}, {}
-for row in io.lines() do
-    r = {}
-    for c in row:gmatch('.') do table.insert(r, c) end
-    table.insert(rows, r)
-    table.insert(save, r)
-end
-function adj(x, y)
-    q = 0
-    for a = -1, 1 do
-        for b = -1, 1 do
-            if a ~= 0 or b ~= 0 then
-                c, d = y + a, x + b
-                while see and rows[c] and rows[c][d] == '.' do
-                    c, d = c + a, d + b
-                end
-                if rows[c] and rows[c][d] == '#' then q = q + 1 end
-            end
-        end
-    end
-    return q
-end
-rule = 4
-function step()
-    local t, v = 0, 0
-    next = {}
-    for y = 1, #rows do
-        next[y] = {}
-        for x = 1, #rows[y] do
-            n = adj(x, y)
-            if rows[y][x] == 'L' and n == 0 then
-                next[y][x] = '#'
-                t = t + 1
-            elseif rows[y][x] == '#' and n >= rule then
-                next[y][x] = 'L'
-                t = t + 1
-            else
-                next[y][x] = rows[y][x]
-            end
-            if next[y][x] == '#' then v = v + 1 end
-        end
-    end
-    rows = next
-    return t, v
-end
-repeat delta, z = step() until delta == 0
-rule, see, rows = 5, true, save
-repeat delta, w = step() until delta == 0
-print(z, w)
+n,e,f=table.insert,{},{}for d in io.lines()do r={}for
+e in d:gmatch('.')do n(r,e)end n(e,r)n(f,r)end function
+h(r,o)q=0 for n=-1,1 do for t=-1,1 do if n~=0 or t~=0
+then c,d=o+n,r+t while i and e[c]and e[c][d]=='.'do
+c,d=c+n,d+t end if e[c]and e[c][d]=='#'then q=q+1 end
+end end end return q end k=4 repeat t,v,j=nil,0,{}for
+d=1,#e do j[d]={}for o=1,#e[d]do n=h(o,d)if e[d][o]=='L'and
+n==0 then j[d][o]='#'t=1 elseif e[d][o]=='#'and n>=k then
+j[d][o]='L't=1 else j[d][o]=e[d][o]end v=v+(j[d][o]=='#'and
+1 or 0)end end e=j if not t and not i then
+k,i,e,t,z=5,1,f,1,v end until not t print(z,v)
