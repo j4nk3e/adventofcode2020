be55cb7728f458bc5521e4399914f0d71d26e90c
Julian Köpke <julian.koepke@gmail.com>
2020-12-15 19:38:20 +0100

    a15 golf

--- a15.lua
+++ a15.lua
@@ -1,15 +1,3 @@
-i, p, m = 0, 0, {}
-function n()
-    s = m[p]
-    m[p], i = i, i + 1
-end
-for t in io.read '*a':gmatch '%d+' do
-    p = t + 0
-    n()
-end
-while i < 30000000 do
-    p = s and i - s - 1 or 0
-    n()
-    b = i == 2020 and p or b
-end
-print(p, b)
+i,p,m=0,0,{}function n()s=m[p]m[p],i=i,i+1 end for i in
+io.read'*a':gmatch'%d+'do p=i+0 n()end while i<3E7 do
+p=s and i-s-1 or 0 n()b=i==2020 and p or b end print(p,b)
