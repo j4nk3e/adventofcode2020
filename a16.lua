66c21889e9c46746b747fe4b12ff962171e5f28d
Julian Köpke <julian.koepke@gmail.com>
2020-12-17 03:51:43 +0100

    a16 golf

--- a16.lua
+++ a16.lua
@@ -1,92 +1,24 @@
-local valid, errorRate, r = nil, nil, {}
-local d = 'departure'
-local e = '%d+'
-
-local function checkRule(rule, n)
-    for name, part in pairs(rule.rules) do
-        if part[1] <= n and part[2] >= n then return name end
-    end
-end
-
-local function check(n)
-    for _, rules in pairs(r) do if checkRule(rules, n) then return true end end
-end
-
-local function insertValid(str)
-    local i = 1
-    local t = {}
-    table.insert(valid, t)
-    for num in str:gmatch(e) do
-        t[i] = num + 0
-        i = i + 1
-    end
-end
-
-local i = 0
-for line in io.lines() do
-    local name, rules = line:gmatch('(%P+): ([^\n]+)')()
-    if rules then
-        for from, to in (rules .. ' or'):gmatch('(%d+)-(%d+) or') do
-            if not r[name] then r[name] = {rules = {}} end
-            table.insert(r[name].rules, {from + 0, to + 0})
-        end
-        i = i + 1
-    elseif line:find('your.*') then
-        valid = {}
-    elseif line:find('nearby.*') then
-        errorRate = 0
-    elseif errorRate then
-        local ok = true
-        for num in line:gmatch(e) do
-            if not check(num + 0) then
-                errorRate = errorRate + num
-                ok = false
-            end
-        end
-        if ok then insertValid(line) end
-    elseif valid and line:find(e) then
-        insertValid(line)
-    end
-end
-for _, rule in pairs(r) do
-    rule.candidates = {}
-    for n = 1, i do rule.candidates[n] = true end
-end
-
-local function count(candidates)
-    local c, last = 0, 0
-    for n, ok in pairs(candidates) do
-        if ok then
-            c = c + 1
-            last = n
-        end
-    end
-    return c, last
-end
-
-local function search()
-    for name, rule in pairs(r) do
-        if name:find(d) and not rule.i then return true end
-    end
-end
-
-while search() do
-    search()
-    for _, rule in pairs(r) do
-        for _, ticket in pairs(valid) do
-            for n = 1, #ticket do
-                if not checkRule(rule, ticket[n]) then
-                    rule.candidates[n] = nil
-                end
-            end
-        end
-        local c, last = count(rule.candidates)
-        if c == 1 then
-            rule.i = last
-            for _, del in pairs(r) do del.candidates[last] = false end
-        end
-    end
-end
-local n = 1
-for name, rule in pairs(r) do if name:find(d) then n = n * valid[1][rule.i] end end
-print(errorRate, n)
+r={}d='departure'e='%d+'function z(n,e)for r,n in
+pairs(n.j)do if n[1]<=e and n[2]>=e then return 1
+end end end function y(n)for _,e in pairs(r)do if
+z(e,n)then return 1 end end end function
+x(n)w,t=1,{}v[#v+1]=t for n in n:gmatch(e)do
+t[w]=n+0 w=w+1 end end i=0 for n in io.lines()do
+g,j=n:gmatch'(%P+): ([^\n]+)'()if j then for e,n
+in(j..' or'):gmatch'(%d+)-(%d+) or'do
+if not r[g]then r[g]={j={}}end
+table.insert(r[g].j,{e+0,n+0})end i=i+1 elseif
+n:find'^y'then v={}elseif n:find'^n'then p=0
+elseif p then ok=1 for n in n:gmatch(e)do if not
+y(n+0)then p=p+n ok=h end end if ok then x(n)end
+elseif v and n:find(e)then x(n)end end for _,n
+in pairs(r)do n.z={}for e=1,i do n.z[e]=1 end end
+function o(n)c,l=0,0 for e,n in pairs(n)do if n
+then c=c+1 l=e end end return c,l end function
+b()for e,n in pairs(r)do if e:find(d)and not n.i
+then return 1 end end end while b()do b()for e,n
+in pairs(r)do for r,e in pairs(v)do for r=1,#e do
+if not z(n,e[r])then n.z[r]=h end end end
+c,l=o(n.z)if c==1 then n.i=l for _,n in pairs(r)do
+n.z[l]=h end end end end n=1 for r,e in pairs(r)do
+if r:find(d)then n=n*v[1][e.i]end end print(p,n)
