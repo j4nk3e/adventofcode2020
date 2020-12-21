d8cb9431c925cc319f3bbfa3e3945383ab22b090
Julian Köpke <julian.koepke@gmail.com>
2020-12-21 08:34:59 +0100

    a18 cleanup

--- a18.lua
+++ a18.lua
@@ -1,4 +1,4 @@
 function e(n)return not n:find'%p'and n or n:find'%('and
 e(n:gsub('%b()',function(n)return e(n:sub(2,-2))end))or
 e(n:gsub('(%d+) (%p) (%d+)',function(e,d,n)return d=='*'and e*n or
-e+n end,1))end s=0 for n in io.lines()do s=s+e(n)end print(s)
+e+n end,1))end results=0 for n in io.lines()do results=results+e(n)end print(results)
--- a18lpeg.lua
+++ a18lpeg.lua
@@ -1,8 +1,8 @@
 require 'lpeg'
 -- Lexical Elements
-local Space = lpeg.S(" \n\t") ^ 0
-local Number = lpeg.C(lpeg.P "-" ^ -1 * lpeg.R("09") ^ 1) * Space
-local TermOp = lpeg.C(lpeg.S("+-*/")) * Space
+local Space = lpeg.S(' ') ^ 0
+local Number = lpeg.C(lpeg.R("09") ^ 1) * Space
+local TermOp = lpeg.C(lpeg.S("+*")) * Space
 local Open = "(" * Space
 local Close = ")" * Space
 
@@ -18,7 +18,7 @@ G = Space * G * -1
 
 -- Evaluator
 function eval(x)
-    if type(x) == "string" then
+    if type(x) == 'string' then
         return tonumber(x)
     else
         local op1 = eval(x[1])
@@ -27,30 +27,14 @@ function eval(x)
             local op2 = eval(x[i + 1])
             if (op == "+") then
                 op1 = op1 + op2
-            elseif (op == "-") then
-                op1 = op1 - op2
             elseif (op == "*") then
                 op1 = op1 * op2
-            elseif (op == "/") then
-                op1 = op1 / op2
             end
         end
         return op1
     end
 end
 
--- Parser/Evaluator
-function evalExp(s)
-    local t = lpeg.match(G, s)
-    if not t then error("syntax error", 2) end
-    return eval(t)
-end
-
 sum = 0
-for line in io.lines() do
-    e = evalExp(line)
-    sum = sum + e
-    print(line, '=', e, '=>', sum)
-end
+for line in io.lines() do sum = sum + eval(lpeg.match(G, line)) end
 print(sum)
-
